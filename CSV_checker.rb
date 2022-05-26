2c893406c57e8edbb113911160efa69ed57fa0f3
Dan <en07djc@icloud.com>
2022-05-26 15:40:10 +0100

    Fixed data clean function and tested.

--- CSV_checker.rb
+++ CSV_checker.rb
@@ -2,30 +2,18 @@ require 'csv'
 
 # load CSV and convert to array of hashes
 
-array_of_hashes = CSV.parse(File.read("MOCK_DATA.csv"), headers: true).map(&:to_h)
+data = CSV.parse(File.read("MOCK_DATA.csv"), headers: true).map(&:to_h)
 
 # iterate through array and delete any hashes with blank values (except phone_number)
 
 def clean_data(array)
+  clean = []
   array.each do |hash|
-    hash.each do |k, v|
-      if v == nil
-        if k != "phone_number"
-          array.delete(hash)
-        end
-      end
+    if hash["first_name"] != nil && hash["last_name"] != nil && hash["email"] != nil
+      clean.push(hash)
     end
+  puts clean
   end
 end
 
-clean_hash = clean_data(array_of_hashes)
-
-# write to clean CSV
-
-CSV.open("cleaned_data.csv", "w") { |csv|
-  headers = clean_hash.flat_map(&:keys).uniq
-  csv << headers
-  clean_hash.each { |row|
-    csv << row.values_at(*headers)
-  }
-}
+clean_data(data)

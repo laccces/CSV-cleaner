0d864119537194d67444de8653278ceab10f6867
Dan <en07djc@icloud.com>
2022-05-26 10:29:59 +0100

    Imported CSV, created cleaner function, created cleaned_data.csv.

---
+++ CSV_checker.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+# load CSV and convert to array of hashes
+
+array_of_hashes = CSV.parse(File.read("MOCK_DATA.csv"), headers: true).map(&:to_h)
+
+# iterate through array and delete any hashes with blank values (except phone_number)
+
+clean_hash = array_of_hashes.each do |hash|
+  hash.each do |k, v|
+    if v == nil
+      if k != "phone_number"
+        array_of_hashes.delete(hash)
+      end
+   end
+  end
+end
+
+# write to clean CSV
+
+CSV.open("cleaned_data.csv", "w") { |csv|
+  headers = clean_hash.flat_map(&:keys).uniq
+  csv << headers
+  clean_hash.each { |row|
+    csv << row.values_at(*headers)
+  }
+}

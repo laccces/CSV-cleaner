4f5332d4f1bd697cef97c3c9091cb9ae03665abb
Dan <en07djc@icloud.com>
2022-05-26 10:39:30 +0100

    Created a method for cleaning the data.

--- CSV_checker.rb
+++ CSV_checker.rb
@@ -6,15 +6,19 @@ array_of_hashes = CSV.parse(File.read("MOCK_DATA.csv"), headers: true).map(&:to_
 
 # iterate through array and delete any hashes with blank values (except phone_number)
 
-clean_hash = array_of_hashes.each do |hash|
-  hash.each do |k, v|
-    if v == nil
-      if k != "phone_number"
-        array_of_hashes.delete(hash)
+def clean_data(array)
+  array.each do |hash|
+    hash.each do |k, v|
+      if v == nil
+        if k != "phone_number"
+          array.delete(hash)
+        end
       end
     end
   end
 end
+
+clean_hash = clean_data(array_of_hashes)
 
 # write to clean CSV
 
